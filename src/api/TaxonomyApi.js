74dc388c01892a1354a812cf8088e912632cefb9
railken <railken@protonmail.ch>
2018-08-01 17:08:50 +0200

    cleanup

--- src/api/TaxonomyApi.js
+++ src/api/TaxonomyApi.js
@@ -5,9 +5,4 @@ Vue.use(VueResource);
 
 export class TaxonomyApi extends ResourceApi {
 	resource_url = '/admin/taxonomies';
-
-	constructor (params) {
-	  super();
-	  this.params = params;
-	}
 };
