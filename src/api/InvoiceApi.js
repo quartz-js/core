e32dfef797b1b8ee1e3b2a0f15fed35fe5c84db7
railken <railken@protonmail.ch>
2018-08-01 17:02:40 +0200

    fix issue invoice

--- src/api/InvoiceApi.js
+++ src/api/InvoiceApi.js
@@ -4,16 +4,16 @@ import { ResourceApi } from './ResourceApi'
 Vue.use(VueResource);
 
 export class InvoiceApi extends ResourceApi {
-resource_url = '/admin/invoices';
+	resource_url = '/admin/invoices';
 
-/**
-* Issue
-*
-* @param {int} id
-*
-* @return {Promise}
-*/
-issue (id) {
-  return Vue.http.post(this.getFullUrl() + '/' + id + '/issue', { headers: { Authorization: 'Bearer ' + this.access_token }});
-}
+	/**
+	* Issue
+	*
+	* @param {int} id
+	*
+	* @return {Promise}
+	*/
+	issue (id) {
+	  return Vue.http.post(this.getFullUrl() + '/' + id + '/issue', {}, { headers: { Authorization: 'Bearer ' + this.access_token }});
+	}
 };
