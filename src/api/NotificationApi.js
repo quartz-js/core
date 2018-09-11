0b60d5f3793d4ddbc204781b5302b07b0c2d6aba
railken <railken@protonmail.ch>
2018-09-11 03:41:54 +0200

    fix notification && cleanup

--- src/api/NotificationApi.js
+++ src/api/NotificationApi.js
@@ -6,18 +6,18 @@ Vue.use(VueResource);
 export class NotificationApi extends ResourceApi {
 resource_url = '/notifications';
 
-/**
+  /**
    * Index
    *
    * @param {Object} params
    *
    * @return {Promise}
    */
-index (params) {
-  return Vue.http.get(this.getFullUrl(), { params: params, headers: { Authorization: 'Bearer ' + this.access_token }});
-}
+  index (params) {
+    return Vue.http.get(this.getFullUrl(), { params: params, headers: { Authorization: 'Bearer ' + this.access_token }}).then(this.parse);
+  }
 
-/**
+  /**
    * Index
    *
    * @param {int} id
@@ -25,29 +25,29 @@ index (params) {
    *
    * @return {Promise}
    */
-show (id, params) {
-  return Vue.http.get(this.getFullUrl() + '/' + id, { params: params, headers: { Authorization: 'Bearer ' + this.access_token }});
-}
+  show (id, params) {
+    return Vue.http.get(this.getFullUrl() + '/' + id, { params: params, headers: { Authorization: 'Bearer ' + this.access_token }});
+  }
 
-/**
+  /**
    * Mark as read
    *
    * @param {int} id
    *
    * @return {Promise}
    */
-markAsRead (id) {
-  return Vue.http.post(this.getFullUrl() + '/' + id + '/read', {}, { headers: { Authorization: 'Bearer ' + this.access_token }});
-}
+  markAsRead (id) {
+    return Vue.http.post(this.getFullUrl() + '/' + id + '/read', {}, { headers: { Authorization: 'Bearer ' + this.access_token }});
+  }
 
-/**
+  /**
    * Mark as read
    *
    * @param {int} id
    *
    * @return {Promise}
    */
-markAsUnread (id) {
-  return Vue.http.post(this.getFullUrl() + '/' + id + '/unread', {}, { headers: { Authorization: 'Bearer ' + this.access_token }});
-}
+  markAsUnread (id) {
+    return Vue.http.post(this.getFullUrl() + '/' + id + '/unread', {}, { headers: { Authorization: 'Bearer ' + this.access_token }});
+  }
 };
