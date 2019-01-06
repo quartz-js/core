e95126f1a8a5a25e39d8b6dcace01af35fab1026
railken <railken@protonmail.ch>
2019-01-06 07:47:51 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 module.exports = {
-	BaseAttribute: require('./attributes/BaseAttribute').BaseAttribute,
-	BelongsToAttribute: require('./attributes/BelongsToAttribute').BelongsToAttribute,
-	DateTimeAttribute: require('./attributes/DateTimeAttribute').DateTimeAttribute,
-	IdAttribute: require('./attributes/IdAttribute').IdAttribute,
-	ImageAttribute: require('./attributes/ImageAttribute').ImageAttribute,
-	SelectAttribute: require('./attributes/SelectAttribute').SelectAttribute,
-	SwitchAttribute: require('./attributes/SwitchAttribute').SwitchAttribute,
-	UrlAttribute: require('./attributes/UrlAttribute').UrlAttribute
+	Attributes: {
+		Base: require('./attributes/BaseAttribute').BaseAttribute,
+		BelongsTo: require('./attributes/BelongsToAttribute').BelongsToAttribute,
+		DateTime: require('./attributes/DateTimeAttribute').DateTimeAttribute,
+		Id: require('./attributes/IdAttribute').IdAttribute,
+		Image: require('./attributes/ImageAttribute').ImageAttribute,
+		Select: require('./attributes/SelectAttribute').SelectAttribute,
+		Switch: require('./attributes/SwitchAttribute').SwitchAttribute,
+		Url: require('./attributes/UrlAttribute').UrlAttribute,
+	},
+	ResourceConfig: require('./resources/ResourceConfig').ResourceConfig,
+	ResourceApi: require('./api/ResourceApi').ResourceApi,
+	container: require('./services/container').container,
+    OAuth: require('./services/oauth').OAuth,
+    Guard: require('./middlewares/Guard').default,
+    mixins: {
+        utils: require('./mixins/utils').utils,
+    },
+	install: function (Vue, options) {
+    	Vue.component("QResourceCreate", require('./components/Resource/Create').default)
+    	Vue.component("QResourceEdit", require('./components/Resource/Edit').default)
+    	Vue.component("QResourceIndex", require('./components/Resource/Index').default)
+    	Vue.component("QResourceRemove", require('./components/Resource/Remove').default)
+    	Vue.component("QResourceShow", require('./components/Resource/Show').default)
+
+    	Vue.component("QPageIndex", require('./components/Page/Index').default)
+    	Vue.component("QPageShow", require('./components/Page/Show').default)
+
+    	Vue.component("QShowJson", require('./components/Show/Json').default)
+    	Vue.component("QShowText", require('./components/Show/Text').default)
+
+        Vue.component("QFormJson", require('./components/Form/FormJson').default)
+        Vue.component("QFormHtml", require('./components/Form/FormHtml').default)
+
+    	Vue.component("QBelongsTo", require('./components/Input/BelongsTo').default)
+    	Vue.component("QHtml", require('./components/Input/Html').default)
+    	Vue.component("QJson", require('./components/Input/Json').default)
+    	Vue.component("QSecret", require('./components/Input/Secret').default)
+    	Vue.component("QSelect", require('./components/Input/Select').default)
+    	Vue.component("QSwitch", require('./components/Input/Switch').default)
+    	Vue.component("QText", require('./components/Input/Text').default)
+    	Vue.component("QTextarea", require('./components/Input/Textarea').default)
+	}
 };
