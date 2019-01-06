524470c6ae90f4dd5f6e4f96a3ef0b2767755a61
railken <railken@protonmail.ch>
2019-01-06 06:59:11 +0100

    update index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import { BelongsToAttribute } from './src/attributes/BelongsToAttribute'
-
-exports = {
-	BelongsToAttribute
-};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	BaseAttribute: require('./attributes/BaseAttribute').BaseAttribute,
+	BelongsToAttribute: require('./attributes/BelongsToAttribute').BelongsToAttribute,
+	DateTimeAttribute: require('./attributes/DateTimeAttribute').DateTimeAttribute,
+	IdAttribute: require('./attributes/IdAttribute').IdAttribute,
+	ImageAttribute: require('./attributes/ImageAttribute').ImageAttribute,
+	SelectAttribute: require('./attributes/SelectAttribute').SelectAttribute,
+	SwitchAttribute: require('./attributes/SwitchAttribute').SwitchAttribute,
+	UrlAttribute: require('./attributes/UrlAttribute').UrlAttribute
+};
