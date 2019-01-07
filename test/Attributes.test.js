6e2155b9d0d575e29154746f8ba0a6f543324d50
railken <railken@protonmail.ch>
2019-01-07 15:22:44 +0100

    update tests

--- test/Attributes.test.js
+++ test/Attributes.test.js
@@ -3,5 +3,7 @@ import { Attributes } from '../src/index'
 describe('BaseAttribute', () => {
   test('should be settable', () => {
     expect(new Attributes.Base('foo').getName()).toEqual('foo');
+    expect(new Attributes.Base().setName('foo').getName()).toEqual('foo');
+    expect(new Attributes.Base().setDescription('foo').getDescription()).toEqual('foo');
   });
 });
