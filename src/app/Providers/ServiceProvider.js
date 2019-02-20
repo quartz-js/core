858c9b9192aa16f5d3fab217bd39c65a4937a830
railken <railken@protonmail.ch>
2019-02-21 00:37:54 +0100

    cleanup icons

--- src/app/Providers/ServiceProvider.js
+++ src/app/Providers/ServiceProvider.js
@@ -39,6 +39,11 @@ export class ServiceProvider {
   }
 
   addData(data) {
+
+    if (!data.icon) {
+      data.icon = require('../../assets/data-icon.svg');
+    }
+
     container.get('$quartz.data').push(data);
   }
 
