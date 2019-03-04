272644e7076dbe67e28123265548230c52f1626c
railken <railken@protonmail.ch>
2019-03-04 14:07:09 +0100

    improve Show/Text

--- src/components/Show/Text.vue
+++ src/components/Show/Text.vue
@@ -1,26 +1,34 @@
 <template>
-  <p>
-      <label class="label-show">{{ getAttributeLabel(attribute) }}</label>
-      <span v-if="attribute.resourceConfig && attribute.extractValue(resource)">
-        <router-link :to="attribute.resourceConfig().getRouteShow(attribute.extractValue(resource))" v-html="html" class="show-value">Show</router-link>
+  <p v-if="show">
+    <label class="label-show">{{ getAttributeLabel(attribute) }}</label>
+    <span v-if="attribute.resourceConfig && attribute.extractValue(resource)">
+      <router-link :to="attribute.resourceConfig().getRouteShow(attribute.extractValue(resource))" v-html="html" class="show-value">Show</router-link>
+    </span>
+    <span  v-else>
+      <span>
+        <span v-if="html !== null" class="show-value" v-html="html"/>
       </span>
-      <span  v-else>
-        <span>
-          <span v-if="html !== null" class="show-value" v-html="html"/>
-        </span>
-        <span v-if="html === null" class="show-value font-italic " v-html="$t('$quartz.core.no-information')"/>
-      </span>
+      <span v-if="html === null" class="show-value font-italic " v-html="$t('$quartz.core.no-information')"/>
+    </span>
   </p>
 </template>
 <script>
 
+import { AttributePreMount } from '../../mixins/AttributePreMount'
 import { ResourceLocalization } from '../../mixins/ResourceLocalization'
 
 export default {
   mixins: [
+    AttributePreMount,
     ResourceLocalization
   ],
   props: ['resource', 'value', 'error', 'attribute', 'errors'],
+  mounted() {
+
+    if (!this.canMount()) {
+      return;
+    }
+  },
   computed: {
     html: function () {
       return this.attribute.extractReadableValue(this.resource);
