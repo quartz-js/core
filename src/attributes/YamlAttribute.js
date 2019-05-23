ae1ac294d43cf15899430402d7ec93c1ad8158aa
railken <railken@protonmail.ch>
2019-05-23 23:01:31 +0200

    update priority YamlAttribute

--- src/attributes/YamlAttribute.js
+++ src/attributes/YamlAttribute.js
@@ -1,9 +1,9 @@
 import { BaseAttribute } from './BaseAttribute'
-import moment from 'moment'
 
 export class YamlAttribute extends BaseAttribute {
   constructor (name, options) {
     super(name, options);
+    this.priority = 0;
   }
 
   getClassName() {
