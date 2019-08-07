89eea555a264ece65fa36086765dd2dfca29be72
railken <railken@protonmail.ch>
2019-08-07 15:20:54 +0200

    fix translation

--- src/app/Translator.js
+++ src/app/Translator.js
@@ -15,19 +15,19 @@ export class Translator {
   
   getLocalizationKeyAttribute (resource, attribute, suffix)
   {
-    return this.getLocalizationPrefixData() + resource + '.attributes.' + attribute.label + suffix;
+    return this.getLocalizationPrefixData() + resource + '.attributes.' + attribute.name + '.' + suffix;
   }
   
   translate(attribute, property)
   {
     let translator = container.get('translator');
 
-    if (translator.te(this.getLocalizationKeyAttribute(attribute.manager().name, attribute, '.' + property))) {
-      return translator.t(this.getLocalizationKeyAttribute(attribute.manager().name, attribute, '.' + property))
+    if (translator.te(this.getLocalizationKeyAttribute(attribute.manager().name, attribute, property))) {
+      return translator.t(this.getLocalizationKeyAttribute(attribute.manager().name, attribute, property))
     }
 
-    if (translator.te(this.getLocalizationKeyAttribute('__common', attribute, '.' + property))) {
-      return translator.t(this.getLocalizationKeyAttribute('__common', attribute, '.' + property))
+    if (translator.te(this.getLocalizationKeyAttribute('__common', attribute, property))) {
+      return translator.t(this.getLocalizationKeyAttribute('__common', attribute, property))
     }
 
     return this.humanize(attribute.name)
