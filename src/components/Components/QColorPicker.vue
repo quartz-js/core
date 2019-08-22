01091b9c3d0d58f78be2977100e013edebba1489
railken <railken@protonmail.ch>
2019-08-22 13:39:29 +0200

    switch between modes

--- src/components/Components/QColorPicker.vue
+++ src/components/Components/QColorPicker.vue
@@ -4,7 +4,7 @@
       <template v-slot:activator="{ on, attrs }">
         <v-btn :disabled="$attrs.disabled" v-on="on" v-bind:style="{background: $attrs.value}" style='border: 1px solid #cacaca' text class="ma-0" small width='28' height='28' min-width='0'></v-btn>
       </template>
-      <v-color-picker v-model="attributes.value" mode="hexa" :show-swatches="true" :hide-mode-switch="true" @input="$emit('input', attributes.value)"></v-color-picker>
+      <v-color-picker v-model="attributes.value" mode="hexa" :show-swatches="true" @input="$emit('input', attributes.value)"></v-color-picker>
     </v-menu>
     <v-subheader>{{ $attrs.label }}</v-subheader>
   </v-row>
