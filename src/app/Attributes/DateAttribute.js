dcac60b4e99d20e107a89ee588fb61b091f8afb7
Railken <railken@protonmail.ch>
2020-02-26 21:27:14 +0100

    cleanup

--- src/app/Attributes/DateAttribute.js
+++ src/app/Attributes/DateAttribute.js
@@ -2,16 +2,6 @@ import { BaseAttribute } from './BaseAttribute'
 import moment from 'moment'
 
 export class DateAttribute extends BaseAttribute {
-  constructor (name, options) {
-    super(name, options);
-    this.mutator = function (value) {
-      value = this.extractor(value);
-      
-      return value ? moment(value).format('DD-MM-YYYY') : null
-    };
-    this.priority = 0;
-  }
-
   getClassName() {
     return 'DateAttribute'
   }
--- src/app/Attributes/DateTimeAttribute.js
+++ src/app/Attributes/DateTimeAttribute.js
@@ -2,16 +2,6 @@ import { BaseAttribute } from './BaseAttribute'
 import moment from 'moment'
 
 export class DateTimeAttribute extends BaseAttribute {
-  constructor (name, options) {
-    super(name, options);
-    this.mutator = function (value) {
-      value = this.extractor(value);
-      
-      return value ? moment(value).format('DD-MM-YYYY[,] HH:mm:ss') : null
-    };
-    this.priority = 0;
-  }
-
   getClassName() {
     return 'DateTimeAttribute'
   }
