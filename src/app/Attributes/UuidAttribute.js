03a33210703a5bfe01e5a9b2dd4726b03ed9fd88
Railken <railken@protonmail.ch>
2020-04-23 22:13:17 +0200

    remove old uuid constructor

--- src/app/Attributes/UuidAttribute.js
+++ src/app/Attributes/UuidAttribute.js
@@ -2,14 +2,6 @@ import { BaseAttribute } from './BaseAttribute'
 
 export class UuidAttribute extends BaseAttribute {
   simple = true
-  
-  constructor () {
-    super('uuid', {});
-    this.extractor = function (resource) {
-      return _.get(resource, 'uuid');
-    };
-    this.priority = 0;
-  }
 
   getClassName() {
     return 'UuidAttribute'
