b45861c28fbd879377f8a5a135b3512065b59635
Railken <railken@protonmail.ch>
2020-04-23 22:13:28 +0200

    remove def image loading icon

--- src/components/Components/QViewIcon.vue
+++ src/components/Components/QViewIcon.vue
@@ -1,25 +1,11 @@
 <template>
-    <v-img v-bind='attributes' :src="src" v-on='$listeners' @error="viewDefault"/>
-  </div>
+  <v-img v-bind='attributes' v-on='$listeners'/>
 </template>
 <script>
 
 import QElement from './QElement'
 
 export default {
-  mixins: [QElement],
-  data() {
-    return {
-      src: null
-    }
-  },
-  methods: {
-    viewDefault() {
-      this.src = require('../../assets/view-default.svg')
-    }
-  },
-  mounted() {
-    this.src = this.$attrs.src;
-  }
+  mixins: [QElement]
 }
 </script>
